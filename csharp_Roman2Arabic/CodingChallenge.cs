73fe350c0fe4ac53e7245cdcb00715a408a3a421
ctrigger <hexadecimal.exe@gmail.com>
2021-07-28 11:07:57 +0100

    First Challenge

---
+++ csharp_Roman2Arabic/CodingChallenge.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+
+namespace Interview
+{
+    public class CodingChallenge
+    {
+        [Fact]
+        public void Test()
+        {
+            RomanTranslator test = new RomanTranslator("VI");
+            test.ConvertRomanToArabic();
+            Assert.Equal(6, test.Value);
+
+            test = new RomanTranslator("LXX");
+            test.ConvertRomanToArabic();
+            Assert.Equal(70, test.Value);
+            
+            test = new RomanTranslator("MCC");
+            test.ConvertRomanToArabic();
+            Assert.Equal(1200, test.Value);
+
+            test = new RomanTranslator("IV");
+            test.ConvertRomanToArabic();
+            Assert.Equal(4, test.Value);
+
+            test = new RomanTranslator("XC");
+            test.ConvertRomanToArabic();
+            Assert.Equal(90, test.Value);
+
+            test = new RomanTranslator("CM");
+            test.ConvertRomanToArabic();
+            Assert.Equal(900, test.Value);
+
+            test = new RomanTranslator("XCV");
+            test.ConvertRomanToArabic();
+            Assert.Equal(95, test.Value);
+
+
+            test = new RomanTranslator("XCIX");
+            test.ConvertRomanToArabic();
+            Assert.Equal(99, test.Value);
+
+            test = new RomanTranslator("XX");
+            test.ConvertRomanToArabic();
+            Assert.Equal(20, test.Value);
+
+            test = new RomanTranslator("XXX");
+            test.ConvertRomanToArabic();
+            Assert.Equal(30, test.Value);
+
+        }
+
+    }
+}
---
+++ csharp_Roman2Arabic/RomanTranslator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interview
+{
+
+    public enum RomanMap
+    {
+        I = 1,
+        V = 5,
+        X = 10,
+        L = 50,
+        C = 100,
+        D = 500,
+        M = 1000
+
+    }
+
+    class RomanTranslator
+    {
+        #region variables
+        private string Input { get; set; }
+        public int Value { get; set; }
+
+        #endregion
+        #region Ctor
+        public RomanTranslator(string _input)
+        {
+            this.Input = _input;
+            this.Value = 0;
+        }
+
+        #endregion
+
+        public void ConvertRomanToArabic()
+        {
+            int i = 0;
+            int j = 0;
+            bool first = true;
+            foreach (char item in this.Input)
+            {
+
+                if (first)
+                {
+                    first = false;
+                    j = (int)Enum.Parse(typeof(RomanMap), item.ToString());
+                    
+                }
+                else
+                {
+                    i = j;
+                    j = (int)Enum.Parse(typeof(RomanMap), item.ToString());
+                    
+                    if (i < j)
+                    {
+                        this.Value -= i;
+                    }
+                    else
+                    {
+                        this.Value += i;
+                    }
+                }
+            }
+            this.Value += j;
+
+        }
+    }
+}
